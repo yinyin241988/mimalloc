5bbccc261660f582626ee831adc86274598eb314
yinyin241988 <59675437+yinyin241988@users.noreply.github.com>
2020-01-09 12:19:13 +0800

    Update heap.c

--- src/heap.c
+++ src/heap.c
@@ -9,7 +9,7 @@ terms of the MIT license. A copy of the license can be found in the file
 #include "mimalloc-internal.h"
 #include "mimalloc-atomic.h"
 
-#include <string.h>  // memset, memcpy
+#include <string.h> ; // memset, memcpy
 
 
 /* -----------------------------------------------------------
